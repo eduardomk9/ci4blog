b47bcf157c5b87f10736c45920831aee4b70f58b
Francisco Eduardo Machado de Holanda <holanda.19@teleperformance.com.br>
2024-06-01 03:09:35 -0300

    feature| login, logout and filter

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Libraries\CIAuth;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class AdminController extends BaseController
@@ -13,5 +14,10 @@ class AdminController extends BaseController
             'pageTitle'=>'Dashboard'
         ];
         return view('backend/pages/home', $data);
+    }
+
+    public function logoutHandler(){
+        CIAuth::forget();
+        return redirect()->route('admin.login.form')->with('fail','You are logged out!');
     }
 }
